13f6dd3c3dd2b98b58253a7a80195a61df332aa6
framp <framp@linux-tips-and-tricks.de>
2017-03-12 10:43:42 +0100

    Bink all LEDs in sequence

--- blinkAllLEDs/blinkAllLEDs.go
+++ blinkAllLEDs/blinkAllLEDs.go
@@ -1,13 +1,11 @@
-// Samples used in a small go tutorial
+package main // Samples used in a small go tutorial
 //
 // Turns on and off all GPIOs in sequence
 //
 // Modified sample blink program from https://github.com/mrmorphic/hwio/tree/master/examples
 // For more samples see https://github.com/framps/golang_tutorial
 //
-// Copyright (C) 2017 framp at linux-tips-and-tricks dot de
-
-package main
+// Copyright (C) 2017 framp at linux-tips-and-tricks dot depackage main
 
 import (
 	"fmt"
@@ -16,46 +14,50 @@ import (
 	"github.com/mrmorphic/hwio"
 )
 
+const (
+	sleepTime  = 250 // time to sleep between blink on and off
+	blinkCount = 3   // number of blinks per run
+)
+
 func main() {
 
-	pinNames := []string{"gpio4", "gpio17", "gpio18", "gpio22", "gpio23", "gpio24", "gpio25", "gpio27"}
+	pins := []string{"gpio4", "gpio17", "gpio18", "gpio22", "gpio23", "gpio24", "gpio25", "gpio27"}
 
 	var (
-		pin hwio.Pin
-		err error
+		ledPin hwio.Pin
+		err    error
 	)
 
-	pins := make([]hwio.Pin, 0)
+	// close everythin at pgm end
+	defer hwio.CloseAll()
 
-	for _, pinName := range pinNames {
+	// endless loop
+	for {
 
-		// get a pin by name. You could also just use the logical pin number, but this is
-		// more readable. On BeagleBone, USR0 is an on-board LED.
-		if pin, err = hwio.GetPin(pinName); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		// loop thorugh all GPIOs
+		for _, pin := range pins {
 
-		// Set the mode of the pin to output. This will return an error if, for example,
-		// we were trying to set an analog input to an output.
-		if err = hwio.PinMode(pin, hwio.OUTPUT); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		pins = append(pins, pin)
-	}
-
-	for _, pin := range pins {
-
-		for {
-			for i := 0; i < 3; i++ {
-				fmt.Printf("Blinking %v pin %v \n", pin)
-				hwio.DigitalWrite(pin, hwio.HIGH)
-				hwio.Delay(500)
-				hwio.DigitalWrite(pin, hwio.LOW)
-				hwio.Delay(500)
+			// get a pin by name. You could also just use the logical pin number, but this is
+			// more readable. On BeagleBone, USR0 is an on-board LED.
+			if ledPin, err = hwio.GetPin(pin); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
 			}
+
+			// Set the mode of the pin to output. This will return an error if, for example,
+			// we were trying to set an analog input to an output.
+			if err = hwio.PinMode(ledPin, hwio.OUTPUT); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			for j := 0; j < blinkCount; j++ {
+				hwio.DigitalWrite(ledPin, hwio.HIGH)
+				hwio.Delay(sleepTime)
+				hwio.DigitalWrite(ledPin, hwio.LOW)
+				hwio.Delay(sleepTime)
+			}
+			hwio.ClosePin(ledPin)
 		}
 	}
 }

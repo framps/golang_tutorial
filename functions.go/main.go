955c2ce7260cd47e04b9003e9ca33d64d7b72aaf
framp <framp@linux-tips-and-tricks.de>
2017-03-18 12:23:47 +0100

    Improved if test

---
+++ functions.go/main.go
@@ -0,0 +1,25 @@
+// Samples used in a small go tutorial
+//
+// Copyright (C) 2017 framp at linux-tips-and-tricks dot de
+//
+// Samples for go syntax - calculate fibonacci number via recursion
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/framps/golang_tutorial/functions.go/fibonacci"
+)
+
+func main() {
+
+	for i := 0; i < fibonacci.MaxFibonacci+3; i++ {
+		if value, err := fibonacci.Fibonacci(i); err != nil {
+			fmt.Printf("Fibonacci number of %d cannot be calculated: %v\n", i, err)
+		} else {
+			fmt.Printf("Fibonacci number of %d is %d\n", i, value)
+		}
+	}
+
+}

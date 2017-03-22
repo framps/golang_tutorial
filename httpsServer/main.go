049897403a30862d07a77493822068c82df3a769
framp <framp@linux-tips-and-tricks.de>
2017-03-22 21:41:19 +0100

    Initial version

---
+++ httpsServer/main.go
@@ -0,0 +1,26 @@
+package main
+
+// https://github.com/denji/golang-tls
+
+import (
+	// "fmt"
+	// "io"
+	"log"
+	"net/http"
+)
+
+// HelloServer -
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server.\n"))
+	// fmt.Fprintf(w, "This is an example server.\n")
+	// io.WriteString(w, "This is an example server.\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServeTLS(":8443", "server.crt", "server.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}

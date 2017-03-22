3eccd4a2fc2e76c77cdc435c993fe46ec98b6614
framp <framp@linux-tips-and-tricks.de>
2017-03-22 23:10:44 +0100

    Using channels

--- trafficLight/trafficLight.go
+++ trafficLight/trafficLight.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-const debug = true
+const debug = false
 
 const (
 	green = iota
@@ -13,6 +13,8 @@ const (
 	red
 	redyellow
 )
+
+var phaseString = []string{". . N", ". G .", "R . .", "R G ."}
 
 // Phase --
 type Phase struct {
@@ -53,43 +55,54 @@ type TrafficLight struct {
 	state   int
 	ticks   int
 	program Program
+	c       *chan struct{}
+}
+
+// Run --
+func (t *TrafficLight) Run() {
+
+	for {
+		debugMessage("%s: Waiting ...\n", t.name)
+		<-*t.c
+		debugMessage("%s: Advancing ...\n", t.name)
+		t.Advance()
+	}
 }
 
 // Advance --
 func (t *TrafficLight) Advance() {
-	fmt.Printf("Got tick %v\n", t.name)
+	debugMessage("%s: Got tick\n", t.name)
 	if t.ticks++; t.ticks >= t.program.Phases[t.state].Ticks {
 		t.state = (t.state + 1) % len(t.program.Phases)
 		t.ticks = 0
-		fmt.Printf("Advanced %v to %v\n", t.name, t.state)
+		fmt.Printf("%v: %v\n", t.name, phaseString[t.state])
 	}
 }
 
-/*
-// Controller -
-type Controller struct {
-	TrafficLights []TrafficLight
-}
-
-func (c *Controller) Start() {
-
-	for _,light := c.TrafficLights range {
-		go
+func debugMessage(f string, p ...interface{}) {
+	if debug {
+		fmt.Printf(f, p...)
 	}
 }
-*/
 
 func main() {
 
-	var trafficLight1 = TrafficLight{"Trafficlight1", 0, 0, NormalProgram}
-	var trafficLight2 = TrafficLight{"Trafficlight2", 2, 0, NormalProgram}
-/*
-	var controller = Controller{[]TrafficLight{trafficLight1, trafficLight2}}
-*/
-	for {
-		time.Sleep(time.Second * 3)
-		trafficLight1.Advance()
-		trafficLight2.Advance()
+	tick1 := make(chan struct{})
+	tick2 := make(chan struct{})
 
+	var trafficLight1 = TrafficLight{"Trafficlight1", 1, 0, NormalProgram, &tick1}
+	var trafficLight2 = TrafficLight{"Trafficlight2", 3, 0, NormalProgram, &tick2}
+
+	trafficLights := []TrafficLight{trafficLight1, trafficLight2}
+
+	for i := range trafficLights {
+		go trafficLights[i].Run()
+	}
+
+	for {
+		for i := range trafficLights {
+			*trafficLights[i].c <- struct{}{}
+		}
+		time.Sleep(time.Second * 1)
 	}
 }

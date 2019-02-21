3c8fc2f4111352436647fceb62a203c98ed6396c
framp <framp@linux-tips-and-tricks.de>
2019-02-21 23:05:28 +0100

    Mostly done

--- GeoLite2xtables/main.go
+++ GeoLite2xtables/main.go
@@ -7,13 +7,18 @@ package main
 import (
 	"archive/zip"
 	"bufio"
+	"encoding/binary"
 	"fmt"
 	"io"
+	"math/big"
+	"net"
 	"net/http"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
+
+	"github.com/apparentlymart/go-cidr/cidr"
 )
 
 // URL Constants -
@@ -28,6 +33,16 @@ type Country struct {
 	Name string
 }
 
+// CountryBlock -
+type CountryBlock struct {
+	Network                     string
+	GeonameID                   string
+	RegisteredCountryGeonameID  string
+	RepresentedCountryGeonameID string
+	IsAnonymousProxy            bool
+	IsSatelliteProvider         bool
+}
+
 // Countries -
 type Countries map[string]Country
 
@@ -36,7 +51,8 @@ func main() {
 	csvFile, err := DownloadFile(CountrCsvFileURL)
 	HandleError(err)
 
-	files, err := Unzip(csvFile, ".")
+	//	files, err := Unzip(csvFile, ".")
+	_, err = Unzip(csvFile, ".")
 	HandleError(err)
 
 	countryFileName, err := DownloadFile(CountryInfoURL)
@@ -60,7 +76,8 @@ func main() {
 		fmt.Printf("%v\n", c)
 	}
 
-	_ = files
+	ParseCountryBlocks("./GeoLite2-Country-CSV_20190219/GeoLite2-Country-Blocks-IPv4.csv", countries)
+	ParseCountryBlocks("./GeoLite2-Country-CSV_20190219/GeoLite2-Country-Blocks-IPv6.csv", countries)
 }
 
 // HandleError -
@@ -149,4 +166,111 @@ func ParseCountries(fileName string) Countries {
 		HandleError(err)
 	}
 	return countries
+}
+
+// ParseCountryBlocks -
+func ParseCountryBlocks(fileName string, countries Countries) {
+
+	fmt.Printf("Parsing %s\n", fileName)
+
+	file, err := os.Open(fileName)
+	HandleError(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		l := scanner.Text()
+		if strings.HasPrefix(l, "network") {
+			continue
+		}
+
+		// network,geoname_id,registered_country_geoname_id,represented_country_geoname_id,is_anonymous_proxy,is_satellite_provider
+		// 1.0.0.0/24,2077456,2077456,,0,0
+
+		// => "1.0.0.0","1.0.0.255","16777216","16777471","AU","Australia"
+
+		tokens := strings.SplitN(scanner.Text(), ",", -1)
+		c := CountryBlock{tokens[0], tokens[1], tokens[2], tokens[3], tokens[4] != "0", tokens[5] != "0"}
+
+		//fmt.Printf("%#v", c)
+
+		_, ipnet, err := net.ParseCIDR(c.Network)
+		HandleError(err)
+		startIP, endIP := cidr.AddressRange(ipnet)
+
+		cc := getCountry(c, countries)
+		code := cc.Code
+		name := cc.Name
+
+		if isIPv4(startIP.String()) {
+			startInt := ip4toint(startIP)
+			endInt := ip4toint(endIP)
+			fmt.Printf("\"%s\",\"%s\",\"%d\",\"%d\",\"%s\",\"%s\"\n",
+				startIP, endIP, startInt, endInt, code, name)
+		} else {
+			startInt := ip6toint(startIP)
+			endInt := ip6toint(endIP)
+			fmt.Printf("\"%s\",\"%s\",\"%d\",\"%d\",\"%s\",\"%s\"\n",
+				startIP, endIP, startInt, endInt, code, name)
+		}
+
+		//os.Exit(0)
+	}
+
+	if err := scanner.Err(); err != nil {
+		HandleError(err)
+	}
+}
+
+func ip4toint(ip net.IP) uint32 {
+	if len(ip) == 16 {
+		return binary.BigEndian.Uint32(ip[12:16])
+	}
+	return binary.BigEndian.Uint32(ip)
+}
+
+// ipv6Decimal := IP6toInt(net.ParseIP("FE80::0202:B3FF:FE1E:8329"))
+func ip6toint(IPv6Address net.IP) *big.Int {
+	IPv6Int := big.NewInt(0)
+	IPv6Int.SetBytes(IPv6Address.To16())
+	return IPv6Int
+}
+
+func isIPv4(address string) bool {
+	return strings.Count(address, ":") < 2
+}
+
+func isIPv6(address string) bool {
+	return strings.Count(address, ":") >= 2
+}
+
+func getCountry(cb CountryBlock, countries Countries) Country {
+
+	if cb.IsAnonymousProxy {
+		return Country{"A1", "Anonymous Proxy"}
+	}
+	if cb.IsSatelliteProvider {
+		return Country{"A2", "Satellite Provider"}
+	}
+	if len(cb.RepresentedCountryGeonameID) > 0 {
+		code := countries[cb.RepresentedCountryGeonameID].Code
+		name := countries[cb.RepresentedCountryGeonameID].Name
+		return Country{code, name}
+	}
+	if len(cb.RegisteredCountryGeonameID) > 0 {
+		code := countries[cb.RegisteredCountryGeonameID].Code
+		name := countries[cb.RegisteredCountryGeonameID].Name
+		return Country{code, name}
+	}
+	if len(cb.GeonameID) > 0 {
+		code := countries[cb.GeonameID].Code
+		name := countries[cb.GeonameID].Name
+		return Country{code, name}
+	}
+	fmt.Printf("Unknown Geoname ID, panicking. This is a bug.\n")
+	fmt.Printf("ID: %s\n", cb.GeonameID)
+	fmt.Printf("ID Registered: %s\n", cb.RegisteredCountryGeonameID)
+	fmt.Printf("ID Represented %s\n", cb.RepresentedCountryGeonameID)
+	os.Exit(1)
+	return Country{}
 }
